1f48016e7d0323790ed4831eb2c90d8175ab8276
themind <themind1991@gmail.com>
2017-05-14 01:47:04 -0300

    implementando codigo do jogo

--- alura/jogo_forca/jogo_forca.c
+++ alura/jogo_forca/jogo_forca.c
@@ -1,6 +1,44 @@
 #include<stdio.h>
-
+#include<string.h>
 int main(){
 
-	return 0;
+	char palavrasecreta[20];
+
+	sprintf(palavrasecreta,"MELANCIA");
+
+	int acertou = 0;
+	int enforcou = 0;
+	char chutes[26];
+	int tentativas = 0;
+
+	do{
+
+		for(int i=0; i< strlen(palavrasecreta); i++){
+			int achou = 0;
+			for(int j=0; j < tentativas; j++){
+				if(chutes[j] == palavrasecreta [i]){
+					achou = 1;
+					
+					break;
+				}
+			}
+
+			if(achou){
+				printf(" %c ",palavrasecreta[i]);
+			}else{
+				printf("_ ");
+			}
+			
+		}
+
+		printf("\n");
+		char chute;
+		// espaço para não armazenar nada no buffer
+		scanf(" %c",&chute);
+
+		chutes[tentativas] = chute;
+		tentativas++;
+
+	}while(!acertou && !enforcou);
+
 }
